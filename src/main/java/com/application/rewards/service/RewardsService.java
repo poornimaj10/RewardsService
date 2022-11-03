3e7758cc4aacc23635bb6f8be5db6d166e480233
ancpo001 <poornima.ancha@staples.com>
2022-11-03 16:29:24 -0400

    clean up

--- src/main/java/com/application/rewards/service/RewardsService.java
+++ src/main/java/com/application/rewards/service/RewardsService.java
@@ -6,7 +6,6 @@ import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.lang.time.StopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +17,8 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.application.rewards.utils.RewardsUtil;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 
 @Service
 public class RewardsService {
@@ -30,7 +31,10 @@ public class RewardsService {
 	@Autowired
 	RestTemplate restTemplate;
 
-	@Value("${service.order.host}")
+	@Autowired
+	EurekaClient eurekaClient;
+
+	@Value("${service.order.name}")
 	private String orderSvc;
 
 	@Value("${service.order.total.api}")
@@ -41,41 +45,41 @@ public class RewardsService {
 
 	@Value("${rewards.minamount.points}")
 	private int rewardsPoint;
-	
+
 	@Value("${rewards.addonamount}")
 	private int incrementAmount;
 
 	@Value("${rewards.addonamount.points}")
 	private int addonPoint;
-	
+
 	public int fetchActiveRewards(String phone, int months) {
 		LOG.debug("In fetchActiveRewards()");
-		StopWatch watch = new StopWatch();
-		Instant start = Instant.now();
+
+		Application application = eurekaClient.getApplication(orderSvc);
+		String url = application.getInstances().get(0).getHomePageUrl();
+
 		Map pathParams = new HashMap<>();
 		pathParams.put("phoneNumber", phone);
 		pathParams.put("months", months);
-		URI ordSvcUri = UriComponentsBuilder.fromUriString(orderSvc.concat(totalApi))
-		        .buildAndExpand(pathParams)
-		        .toUri();
-		 ResponseEntity<Float> response = restTemplate.exchange(ordSvcUri,
-                HttpMethod.GET, null,Float.class);
+
+		URI ordSvcUri = UriComponentsBuilder.fromUriString(url.concat(totalApi)).buildAndExpand(pathParams).toUri();
+		Instant start = Instant.now();
+		ResponseEntity<Float> response = restTemplate.exchange(ordSvcUri, HttpMethod.GET, null, Float.class);
 		Instant end = Instant.now();
-		LOG.info("Time taken to get the Purchase Total in nanosec {}",Duration.between(end,start).getNano());
+		LOG.info("Time taken to get the Purchase Total in nanosec {}", Duration.between(end, start).getNano());
+		
 		Integer activeRewards;
-		if(response.getStatusCode().is2xxSuccessful() && response.hasBody()) {
-			//Object tmp = response.getBody();
+		if (response.getStatusCode().is2xxSuccessful() && response.hasBody()) {
 			Float tot = response.getBody();
-			//Rounding aggregate to the closest integer
+			// Rounding aggregate to the closest integer
 			activeRewards = calcRewards(Math.round(tot), floorAmount, rewardsPoint, incrementAmount, addonPoint);
-		}else {
-			LOG.error("Time taken to get the Purchase Total in nanosec {}",Duration.between(end,start).getNano());
+		} else {
+			LOG.error("Time taken to get the Purchase Total in nanosec {}", Duration.between(end, start).getNano());
 			activeRewards = -1;
 		}
-		
+
 		return activeRewards;
 	}
-
 
 	public int calcRewards(int total, int floorAmount, int rewardsPoint, int incrementAmount, int addonPoint) {
 		return util.calcRewards(total, floorAmount, rewardsPoint, incrementAmount, addonPoint);
